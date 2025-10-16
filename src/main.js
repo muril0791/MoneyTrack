9495c1496c80aaac613878d0a080992413f1dc1a
Murilo <muriloferreira.santos@hotmail.com>
2025-10-16 03:48:11 -0300

    fix import useMainStore on main.js

--- src/main.js
+++ src/main.js
@@ -4,23 +4,25 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+
 import { supabase } from "@/lib/supabase";
-import { pinia } from "./stores/store";
+import { pinia, useMainStore } from "./stores/store";
 import router from "./router/router";
 import "./index.css";
 import "@mdi/font/css/materialdesignicons.css";
 
 import App from "./App.vue";
 
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+const vuetify = createVuetify({ components, directives });
 
-const app = createApp(App).use(router).use(pinia).use(vuetify);
+const app = createApp(App);
+app.use(router);
+app.use(pinia);
+app.use(vuetify);
+
+const store = useMainStore();
 
 supabase.auth.onAuthStateChange((_event, session) => {
-  const store = useMainStore();
   if (!session) {
     store.reset();
     if (router.currentRoute.value.name !== "Login") {
@@ -33,11 +35,9 @@ app.config.errorHandler = (err, vm, info) => {
   console.error("Vue error handler:", err, info);
 };
 
-
 app.config.warnHandler = (msg, vm, trace) => {
   console.warn("Vue warn handler:", msg, trace);
 };
-
 
 app.directive("trim", {
   beforeMount(el) {
